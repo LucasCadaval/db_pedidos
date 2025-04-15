299660fb0548201d1566f878161fd3cc9e86cd11
Lucas Cadaval <cadavallfc@gmail.com>
2025-04-15 15:18:58 -0300

    feat: Flyway V2 to insert values into brands table

---
+++ src/main/resources/db/migration/V2__Insert_Values_Into_Brand.sql
@@ -0,0 +1,7 @@
+INSERT INTO brands (name, site, phone) VALUES
+   ('Nike', 'https://www.nike.com', '+1 (800) 344-6453'),
+   ('Adidas', 'https://www.adidas.com', '+49 (0)30 255 999 00'),
+   ('Apple', 'https://www.apple.com', '+1 (800) 692-7753'),
+   ('Samsung', 'https://www.samsung.com', '+1 (800) 726-7864'),
+   ('Sony', 'https://www.sony.com', '+1 (800) 222-7669');
+
