0bdd36e2fb080cda2bb9035de211a67b7333b25e
Lucas Cadaval <cadavallfc@gmail.com>
2025-04-15 14:26:22 -0300

    feat: First commit

---
+++ src/main/java/com/lucas/db_pedidos/DbPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.db_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/db_pedidos/DbPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.db_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
