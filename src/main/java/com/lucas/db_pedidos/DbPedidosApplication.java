5d309c51957d6a0ccc48a3d445c7492a9d6179e5
Lucas Cadaval <cadavallfc@gmail.com>
2025-04-15 14:35:20 -0300

    feat: Connection to the H2 database and environment variables

--- src/main/java/com/lucas/db_pedidos/DbPedidosApplication.java
+++ src/main/java/com/lucas/db_pedidos/DbPedidosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DbPedidosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbPedidosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DbPedidosApplication.class, args);
+    }
 
 }
