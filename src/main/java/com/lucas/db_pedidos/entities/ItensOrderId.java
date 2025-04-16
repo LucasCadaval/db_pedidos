7537f2f6610c5fc15ec46e8d90e778103a739761
Lucas Cadaval <cadavallfc@gmail.com>
2025-04-16 20:13:07 -0300

    feat: Created ItensOrderId for Multiple id's in ItensOrder

---
+++ src/main/java/com/lucas/db_pedidos/entities/ItensOrderId.java
@@ -0,0 +1,27 @@
+package com.lucas.db_pedidos.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItensOrderId implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
